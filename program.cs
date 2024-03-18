3523325e278d3d1cc8a26813f3e2435ad2503d73
usersilva <silvianelson2003@gmail.com>
2024-03-18 14:45:57 +0300

    added

---
+++ program.cs
@@ -0,0 +1,16 @@
+namespace lab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public class River
+    {
+        public string name;
+        public double square;
+        public double lenght;
+    }
+}
